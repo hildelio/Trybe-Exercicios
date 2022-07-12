c81decbe2dddaf92a7b1962968fa64545ace87e7
hildelio <hildelio@gmail.com>
2022-07-12 20:46:00 -0300

    finalizando requisitos obrigatórios

--- 01-Fundamentos-de-Desenvolvimento/Bloco06_HTML_CSS:_Forms_Flexbox_Responsivo/Aulaxx_Forms/Projeto_TrybeWarts/script.js
+++ 01-Fundamentos-de-Desenvolvimento/Bloco06_HTML_CSS:_Forms_Flexbox_Responsivo/Aulaxx_Forms/Projeto_TrybeWarts/script.js
@@ -1,9 +1,20 @@
-const form = document.querySelector('.trybewarts-login');
-form.addEventListener('submit', function(event) {
+const login = document.querySelector('.trybewarts-login');
+login.addEventListener('submit', function(event) {
   if (document.getElementById('login').value === 'tryber@test.com' && document.getElementById('senha').value === '123456') {
     event.preventDefault();
     alert('Olá Tryber!')
   } else {
     alert('Email ou senha inválidos.')
+  }
+})
+
+const form = document.getElementById('evaluation-form')
+document.addEventListener('submit', function(event) {
+  const agree = document.getElementById('agreement') 
+  const button = document.getElementById('submit-btn')
+  if (agree.checked) {
+    button.disabled = false
+  } else {
+    button.disabled = true
   }
 })
