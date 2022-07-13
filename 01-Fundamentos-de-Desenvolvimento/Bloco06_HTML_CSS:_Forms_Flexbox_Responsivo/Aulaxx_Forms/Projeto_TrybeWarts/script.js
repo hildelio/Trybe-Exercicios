71b5e7598e9f643e8b66a1a6c5851e7a4280ec63
hildelio <hildelio@gmail.com>
2022-07-13 10:27:27 -0300

    faltando apenas um item bonus avaliativo

--- 01-Fundamentos-de-Desenvolvimento/Bloco06_HTML_CSS:_Forms_Flexbox_Responsivo/Aulaxx_Forms/Projeto_TrybeWarts/script.js
+++ 01-Fundamentos-de-Desenvolvimento/Bloco06_HTML_CSS:_Forms_Flexbox_Responsivo/Aulaxx_Forms/Projeto_TrybeWarts/script.js
@@ -2,19 +2,37 @@ const login = document.querySelector('.trybewarts-login');
 login.addEventListener('submit', function(event) {
   if (document.getElementById('login').value === 'tryber@test.com' && document.getElementById('senha').value === '123456') {
     event.preventDefault();
-    alert('Olá Tryber!')
+    alert('Olá Tryber!');
   } else {
-    alert('Email ou senha inválidos.')
+    alert('Email ou senha inválidos.');
   }
-})
+});
 
-const form = document.getElementById('evaluation-form')
-document.addEventListener('submit', function(event) {
-  const agree = document.getElementById('agreement') 
-  const button = document.getElementById('submit-btn')
+function ableDisableButton() {
+  const agree = document.querySelector('#agreement');
+  const button = document.querySelector('#submit-btn');
   if (agree.checked) {
-    button.disabled = false
+    button.disabled = false;
   } else {
-    button.disabled = true
+    button.disabled = true;
   }
-})
+}
+
+let count = 500;
+let counter = document.getElementById('counter');
+const textarea = document.querySelector('#textarea');
+textarea.addEventListener('keypress', function(event) {
+count = 500 - (textarea.value.length + 1);
+counter.innerText = count + ' caracteres restantes.';
+});
+
+const form = document.querySelector('#evaluation-form');
+form.addEventListener('submit', function(event) {
+  
+  // const all = document.querySelectorAll('#evaluation-form')
+  // for (let i = 0; i < all.length; i += 1) {
+  //   console.log(all[i].value)
+  // }
+  
+  
+});
