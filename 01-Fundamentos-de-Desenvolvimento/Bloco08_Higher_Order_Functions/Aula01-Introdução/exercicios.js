09ee2c3d6abc074f2193195a4538fe96f08ba58f
hildelio <hildelio@gmail.com>
2022-07-23 10:25:55 -0300

    pausa nos estudos pra brincar com Alice

---
+++ 01-Fundamentos-de-Desenvolvimento/Bloco08_Higher_Order_Functions/Aula01-Introdução/exercicios.js
@@ -0,0 +1,13 @@
+// Vamos praticar com os seguintes exercícios:
+// 1 - Crie uma função que retorne a string 'Acordando!!';
+const wakingUp = () => 'Acordando!!';
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!';
+const breakfeast = () => 'Bora tomar café!!';
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!';
+const sleep = () => 'Partiu dormir!!';
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores. Exemplo:
+const doingThings = (func) => func();
+
+console.log(doingThings(wakingUp));
+console.log(doingThings(breakfeast));
+console.log(doingThings(sleep));
